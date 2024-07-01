0712059f9b164e3dcf3d5a31363ddeb67ce30fd1
Mahmoud <mahmoud.abdelshafi4@gmail.com>
2024-07-01 15:47:15 +0300

    Initial Commit

---
+++ TMDB/TMDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBApp.swift
+//  TMDB
+//
+//  Created by Mahmoud Abdelshafi on 01/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TMDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
